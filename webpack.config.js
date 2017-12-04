cb392905cef48181abe704a44f49476567745fb7
shirley <1242663098@qq.com>
2017-12-05 00:47:28 +0800

    lesson2.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path=require('path');
 module.exports={
     entry:{
-        entry: './src/entry.js'
+        entry: './src/entry.js',
+        entry2: './src/entry2.js'
     },
     output:{
         path:path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module:{},
     plugins:[],
